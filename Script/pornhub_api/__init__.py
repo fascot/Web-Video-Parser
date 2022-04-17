cf50a8c7f106ee376e6ea9a45ea8e815b6f79835
fascot <53346081+fascot@users.noreply.github.com>
2022-04-17 14:56:37 +0300

    Delete Script/pornhub_api directory

--- Script/pornhub_api/__init__.py
+++
@@ -1,3 +0,0 @@
-from pornhub_api.api import PornhubApi
-
-__all__ = ("PornhubApi",)
--- Script/pornhub_api/schemas/pornstart.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-__all__ = ("Pornstar",)
-
-
-class Pornstar(BaseModel):
-    pornstar_name: str
